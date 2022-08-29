90d626b942efafe083d33a8999d2a7ddbaf252ad
Yanwei <yanwei.self@gmail.com>
2022-08-29 11:21:18 +0800

    support mmdet3d v1.x
    change box coord to support mmdet3d v1.x

--- projects/mmdet3d_plugin/core/bbox/util.py
+++ projects/mmdet3d_plugin/core/bbox/util.py
@@ -1,4 +1,8 @@
 import torch 
+import numpy as np
+import mmdet3d
+
+__mmdet3d_version__ = float(mmdet3d.__version__[:3])
 
 
 def normalize_bbox(bboxes, pc_range=None):
@@ -6,11 +10,19 @@ def normalize_bbox(bboxes, pc_range=None):
     cx = bboxes[..., 0:1]
     cy = bboxes[..., 1:2]
     cz = bboxes[..., 2:3]
-    w = bboxes[..., 3:4].log()
-    l = bboxes[..., 4:5].log()
-    h = bboxes[..., 5:6].log()
-
-    rot = bboxes[..., 6:7]
+    # align coord system with previous version
+    if __mmdet3d_version__ < 1.0:
+        w = bboxes[..., 3:4].log()
+        l = bboxes[..., 4:5].log()
+        h = bboxes[..., 5:6].log()
+        rot = bboxes[..., 6:7]
+    else:
+        l = bboxes[..., 3:4].log()
+        w = bboxes[..., 4:5].log()
+        h = bboxes[..., 5:6].log()
+        rot = bboxes[..., 6:7]
+        rot = -rot - np.pi / 2
+    
     if bboxes.size(-1) > 7:
         vx = bboxes[..., 7:8] 
         vy = bboxes[..., 8:9]
@@ -23,13 +35,16 @@ def normalize_bbox(bboxes, pc_range=None):
         )
     return normalized_bboxes
 
-def denormalize_bbox(normalized_bboxes, pc_range=None):
+def denormalize_bbox(normalized_bboxes, pc_range=None, version=0.8):
     # rotation 
     rot_sine = normalized_bboxes[..., 6:7]
 
     rot_cosine = normalized_bboxes[..., 7:8]
     rot = torch.atan2(rot_sine, rot_cosine)
-
+    
+    # align coord system with previous version
+    if __mmdet3d_version__ >= 1.0:
+        rot = -rot - np.pi / 2
     # center in the bev
     cx = normalized_bboxes[..., 0:1]
     cy = normalized_bboxes[..., 1:2]
@@ -47,9 +62,15 @@ def denormalize_bbox(normalized_bboxes, pc_range=None):
          # velocity 
         vx = normalized_bboxes[..., 8:9]
         vy = normalized_bboxes[..., 9:10]
-        denormalized_bboxes = torch.cat([cx, cy, cz, w, l, h, rot, vx, vy], dim=-1)
+        if __mmdet3d_version__ < 1.0:
+            denormalized_bboxes = torch.cat([cx, cy, cz, w, l, h, rot, vx, vy], dim=-1)
+        else:
+            denormalized_bboxes = torch.cat([cx, cy, cz, l, w, h, rot, vx, vy], dim=-1)
     else:
-        denormalized_bboxes = torch.cat([cx, cy, cz, w, l, h, rot], dim=-1)
+        if __mmdet3d_version__ < 1.0:
+            denormalized_bboxes = torch.cat([cx, cy, cz, w, l, h, rot], dim=-1)
+        else:
+            denormalized_bboxes = torch.cat([cx, cy, cz, l, w, h, rot], dim=-1)
     return denormalized_bboxes
 
 def bbox3d_mapping_back(bboxes, rot_degree, scale_factor, flip_horizontal, flip_vertical):
